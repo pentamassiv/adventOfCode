265409ac8268be0ef7e09f4be8f7d3f8435d3062
pentamassiv <pentamassiv@posteo.de>
2022-12-04 11:32:14 +0100

    Finished Day 4

--- 2022/aoc224/src/main.rs
+++ 2022/aoc224/src/main.rs
@@ -7,19 +7,28 @@ fn main() {
     // Read the input file
     let lines = io::BufReader::new(File::open("2022/aoc224/input").unwrap()).lines();
 
-    let mut count = 0;
+    let mut complete_overlaps = 0;
+    let mut partial_overlaps = 0;
     // Iterator over the lines
     lines.flatten().for_each(|line| {
         line.splitn(4, |c| c == '-' || c == ',')
             .map(|x| x.parse::<i32>().unwrap())
             .array_chunks::<4>()
             .filter(|[start_a, end_a, start_b, end_b]| {
-                start_a == start_b || end_a == end_b || start_a.cmp(start_b) != end_a.cmp(end_b)
+                (*start_a..=*end_a).contains(start_b)
+                    || (*start_a..=*end_a).contains(end_b)
+                    || (*start_b..=*end_b).contains(start_a)
+                    || (*start_b..=*end_b).contains(end_a)
             })
+            .inspect(|_| {
+                partial_overlaps += 1;
+            })
+            .filter(|[start_a, end_a, start_b, end_b]| start_a.cmp(start_b) != end_a.cmp(end_b))
             .for_each(|_| {
-                count += 1;
+                complete_overlaps += 1;
             })
     });
 
-    println!("{count}");
+    println!("{complete_overlaps}");
+    println!("{partial_overlaps}");
 }
