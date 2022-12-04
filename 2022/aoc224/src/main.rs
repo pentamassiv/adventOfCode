80da83eb749544a9d07da7c7006f2e80d4185846
pentamassiv <pentamassiv@posteo.de>
2022-12-04 09:53:35 +0100

    Weird split_terminator behavior

--- 2022/aoc224/src/main.rs
+++ 2022/aoc224/src/main.rs
@@ -5,8 +5,15 @@ fn main() {
     // Read the input file
     let lines = io::BufReader::new(File::open("2022/aoc224/input").unwrap()).lines();
 
+    let mut count = 0;
     // Iterator over the lines
     for line in lines.flatten() {
-        println!("{}", line);
+        let line: Vec<&str> = line.split_terminator(&['-', ',']).take(4).collect();
+        let mut line = vec![(1, line[0]), (1, line[1]), (2, line[2]), (2, line[3])];
+        line.sort_by_key(|(_, value)| value.parse::<i32>().unwrap());
+        if line[0].0 == line[3].0 {
+            count += 1;
+        }
     }
+    println!("{count}");
 }
