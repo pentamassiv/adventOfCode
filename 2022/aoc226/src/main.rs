7848986eec1b6f3ef800f5ef9ff0cf78d5bb2252
pentamassiv <pentamassiv@posteo.de>
2022-12-06 11:19:20 +0100

    Cleaned up day 6

--- 2022/aoc226/src/main.rs
+++ 2022/aoc226/src/main.rs
@@ -3,33 +3,31 @@
 fn main() {
     // Read the input file
     let input = std::fs::read_to_string("2022/aoc226/input").unwrap();
-    let mut processed_chars = (0, 0);
-    for no in 3..input.len() {
-        if first_n_uneq(&input[no - 3..=no]) > 3 {
-            processed_chars.0 = no + 1;
-            break;
-        }
-    }
-    for no in 13..input.len() {
-        if first_n_uneq(&input[no - 13..=no]) == 14 {
-            processed_chars.1 = no + 1;
-            break;
-        }
-    }
+
+    let mut solution = (0, 0);
+    solution.0 = find_idx_n_distinct_chars(input.as_bytes(), 4).unwrap_or(input.len() + 1);
+    solution.1 = find_idx_n_distinct_chars(input.as_bytes(), 14).unwrap_or(input.len() + 1);
 
     println!("Part 1:");
-    println!("{}", processed_chars.0);
+    println!("{}", solution.0);
     println!("Part 2:");
-    println!("{}", processed_chars.1);
+    println!("{}", solution.1);
 }
 
-fn first_n_uneq(slice: &str) -> usize {
-    for (no, char_a) in slice.chars().enumerate() {
-        for char_b in slice.chars().skip(no + 1) {
+fn find_idx_n_distinct_chars(input: &[u8], n_distinct: usize) -> Option<usize> {
+    input
+        .windows(n_distinct)
+        .enumerate()
+        .find_map(|(no, ch)| (all_distinct(ch)).then_some(no + n_distinct))
+}
+
+fn all_distinct(slice: &[u8]) -> bool {
+    for (no, char_a) in slice.iter().enumerate() {
+        for char_b in slice.iter().skip(no + 1) {
             if char_a == char_b {
-                return no + 1;
+                return false;
             }
         }
     }
-    slice.len()
+    true
 }
