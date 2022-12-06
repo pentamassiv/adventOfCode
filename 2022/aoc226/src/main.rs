4ddb3e156b1ef1dca9b4ada1c22932df293022e5
pentamassiv <pentamassiv@posteo.de>
2022-12-06 07:27:59 +0100

    Improved Day 6

--- 2022/aoc226/src/main.rs
+++ 2022/aoc226/src/main.rs
@@ -5,13 +5,13 @@ fn main() {
     let input = std::fs::read_to_string("2022/aoc226/input").unwrap();
     let mut processed_chars = (0, 0);
     for no in 3..input.len() {
-        if all_uneq(&input[no - 3..=no]) {
+        if first_n_uneq(&input[no - 3..=no]) > 3 {
             processed_chars.0 = no + 1;
             break;
         }
     }
     for no in 13..input.len() {
-        if all_uneq(&input[no - 13..=no]) {
+        if first_n_uneq(&input[no - 13..=no]) == 14 {
             processed_chars.1 = no + 1;
             break;
         }
@@ -23,13 +23,13 @@ fn main() {
     println!("{}", processed_chars.1);
 }
 
-fn all_uneq(slice: &str) -> bool {
+fn first_n_uneq(slice: &str) -> usize {
     for (no, char_a) in slice.chars().enumerate() {
         for char_b in slice.chars().skip(no + 1) {
             if char_a == char_b {
-                return false;
+                return no + 1;
             }
         }
     }
-    true
+    slice.len()
 }
