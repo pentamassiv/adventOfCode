e9c7127b8d9134e6712887f189aae3b6124b068e
pentamassiv <pentamassiv@posteo.de>
2022-12-06 05:02:17 +0100

    Improved Day 1

--- 2022/aoc221/src/main.rs
+++ 2022/aoc221/src/main.rs
@@ -1,32 +1,26 @@
-use std::fs::File;
-use std::io::{self, BufRead};
+use std::fs;
 
 fn main() {
     // Read the input file
-    let lines = io::BufReader::new(File::open("2022/aoc221/input").unwrap()).lines();
+    let mut calories: Vec<i32> = fs::read_to_string("2022/aoc221/input")
+        .unwrap()
+        .split("\n\n")
+        .map(calculate_calories_of_one_elf)
+        .collect();
+    calories.sort();
 
-    let mut max_calories = vec![0, 0, 0];
-    let mut current_calories = 0;
-    let mut pos = 3;
-
-    for line in lines.flatten() {
-        if line.is_empty() {
-            for (no, max_cal) in max_calories.iter().enumerate() {
-                if current_calories > *max_cal {
-                    pos = no;
-                    break;
-                }
-            }
-            max_calories.insert(pos, current_calories);
-            pos = 3;
-            max_calories.pop();
-            current_calories = 0;
-        } else {
-            current_calories += line.parse().unwrap_or(0);
-        }
-    }
     println!("Part 1:");
-    println!("{}", max_calories[0]);
+    println!("{}", calories[calories.len() - 1]);
     println!("Part 2:");
-    println!("{}", max_calories.iter().sum::<i32>());
+    println!(
+        "{}",
+        calories[calories.len() - 1] + calories[calories.len() - 2] + calories[calories.len() - 3]
+    );
+}
+
+fn calculate_calories_of_one_elf(lines_of_one_elf: &str) -> i32 {
+    lines_of_one_elf
+        .split('\n')
+        .filter_map(|str| str.parse::<i32>().ok())
+        .sum()
 }
