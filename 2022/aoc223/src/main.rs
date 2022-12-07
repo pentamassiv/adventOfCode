9c9c87d9bf249a4c472d2592220b5e6e07f71a49
pentamassiv <pentamassiv@posteo.de>
2022-12-07 03:42:21 +0100

    Improved day 3

--- 2022/aoc223/src/main.rs
+++ 2022/aoc223/src/main.rs
@@ -1,43 +1,29 @@
-#![feature(drain_filter)]
 #![warn(clippy::pedantic)]
+
 fn main() {
     // Read the input file
     let input = std::fs::read_to_string("2022/aoc223/input").unwrap();
-
-    let mut compartments;
+    let input = input.lines().collect::<Vec<&str>>();
     let mut sum_prio = 0;
     let mut sum_badge = 0;
-    let mut badge_candidates = vec![];
-    // Iterator over the lines
-    for (no, rucksack) in input.lines().enumerate() {
-        compartments = rucksack.split_at(rucksack.len() / 2);
-        if no % 3 == 0 {
-            for item_type in rucksack.chars() {
-                badge_candidates.push(item_type);
-            }
-        } else {
-            badge_candidates = badge_candidates
-                .drain_filter(|item_type| rucksack.contains(*item_type))
-                .collect::<Vec<_>>();
-        }
-        if no % 3 == 2 {
-            sum_badge += item_prio(badge_candidates[0]);
-            badge_candidates.clear();
-        }
 
-        //println!("{} {}", compartments.0, compartments.1);
-        for item_type in compartments.0.chars() {
-            if compartments.1.contains(item_type) {
-                //println!("{}({})", item_prio(item_type), item_type);
-                sum_prio += item_prio(item_type);
-                break;
-            }
+    // Get groups of three rucksacks
+    for group in input.chunks(3) {
+        // Check each rucksack for the duplicate item type
+        for &rucksack in group {
+            let (l, r) = rucksack.split_at(rucksack.len() / 2);
+            sum_prio += item_prio(duplicate_item(&[l, r][..]));
         }
+        // Check the group of rucksacks for the badge
+        sum_badge += item_prio(duplicate_item(group));
     }
+    println!("Part 1:");
     println!("{sum_prio}");
+    println!("Part 2:");
     println!("{sum_badge}");
 }
 
+// Calculate the prio of an item type
 fn item_prio(item_type: char) -> u32 {
     if item_type.is_lowercase() {
         u32::from(item_type) - u32::from('a') + 1
@@ -47,4 +33,14 @@ fn item_prio(item_type: char) -> u32 {
         print!("Found weird item_type: {item_type}");
         0
     }
+}
+
+// Find the duplicate item type
+fn duplicate_item(slice: &[&str]) -> char {
+    for c in slice[0].chars() {
+        if slice[1..].iter().all(|x| x.contains(c)) {
+            return c;
+        }
+    }
+    panic!("No duplicate found!");
 }
